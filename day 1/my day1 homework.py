5001404705f4f97fbe2c2bce55ad67e7f1c8107f
DatunaGOA <barbakadzedata@gmail.com>
2024-01-23 20:03:02 +0400

    commit

---
+++ day 1/my day1 homework.py
@@ -0,0 +1,7 @@
+name = "My name is Davit" #str
+surname = "Barbakadze, i am" #str
+age = 13 #int
+name1 = "years old, and i am" #str
+height = 154.5 #int
+name2 = "CM tall" #str
+print(name + " " + surname + " " + str(age) + " " + name1 + " " + str(height) + " " + name2)
