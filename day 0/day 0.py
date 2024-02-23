bfc56e235d878736b4bfad1ecd6df8ec8702717f
DatunaGOA <barbakdzedata@gmail.com>
2024-02-23 19:37:41 +0400

    commit

---
+++ day 0/day 0.py
@@ -0,0 +1,172 @@
+from turtle import *
+
+# starting the house 
+
+
+color("green")
+width(6)
+
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+
+#drawing a door on the house
+
+forward(70)
+color("red")
+left(90)
+forward(120)
+left(270)
+forward(60)
+right(90)
+forward(120)
+left(90)
+color("green")
+forward(70)
+left(90)
+
+#finished door finaly
+
+#starting roof now
+
+forward(200)
+color("yellow")
+left(45)
+forward(140)
+left(90)
+forward(142)
+color("green")
+
+#finished roof yay
+ 
+#starting windows yay
+
+color("blue")
+left(45)
+forward(70)
+
+left(90)
+
+forward(70)
+left(90)
+forward(70)
+left(90)
+forward(70)
+left(90)
+color("green")
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+color("blue")
+left(90)
+forward(70)
+left(90)
+forward(70)
+left(90)
+forward(70)
+left(90)
+
+
+
+#windows finished fully
+
+
+
+exitonclick()
+
+
+
+#house fully finished
+
+
+# starting the house 
+
+
+color("green")
+width(6)
+
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+
+#drawing a door on the house
+
+forward(70)
+color("red")
+left(90)
+forward(120)
+left(270)
+forward(60)
+right(90)
+forward(120)
+left(90)
+color("green")
+forward(70)
+left(90)
+
+#finished door finaly
+
+#starting roof now
+
+forward(200)
+color("yellow")
+left(45)
+forward(140)
+left(90)
+forward(142)
+color("green")
+
+#finished roof yay
+ 
+#starting windows yay
+
+color("blue")
+left(45)
+forward(70)
+
+left(90)
+
+forward(70)
+left(90)
+forward(70)
+left(90)
+forward(70)
+left(90)
+color("green")
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+color("blue")
+left(90)
+forward(70)
+left(90)
+forward(70)
+left(90)
+forward(70)
+left(90)
+
+
+
+#windows finished fully
+
+
+
+exitonclick()
+
+
+
+#house fully finished
