9eb111e6ba37e425e01c60b36fce74e65e480eb2
DatunaGOA <barbakdzedata@gmail.com>
2024-02-28 19:46:04 +0400

    commit

---
+++ day 4/day4.py
@@ -0,0 +1,4 @@
+name = input("enter your name: ")
+surname = input("enter your surname: ")
+age = input("enter your age: ")
+print("your  name is " + name + " " +" your surname is " + surname + " and your age is " + age )
