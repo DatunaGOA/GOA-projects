0049ec887b1d584edbb37809af45d0188ffa5620
DatunaGOA <barbakadzedata@gmail.com>
2024-03-11 19:58:29 +0400

    commit

---
+++ day 11/day 11.py
@@ -0,0 +1,37 @@
+#work number 1
+num = [2, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for i in num:
+    print(num[i] * 2)
+num = [2, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for i in num:
+    print(num[i] - 2)
+   
+num = [2, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for i in num:
+    print(num[i] / 2)
+
+num = [2, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for i in num:
+
+    print(num[i] + 2)
+  
+
+#work number 2
+    
+
+ 
+
+animals = ["parrot", "capybara", "lion"]
+
+animals[2] = 'tiger'
+
+print(animals)
+
+animals_1 = ["eagle", "mouse", "pterodactyl"]
+
+animals_1[2] = 'cat'
+
+print(animals_1)
+
+
+
