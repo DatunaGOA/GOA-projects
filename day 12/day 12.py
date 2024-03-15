e27ef76701adff82cd6e3dc1964722e55bb6b1a0
DatunaGOA <barbakadzedata@gmail.com>
2024-03-15 22:15:52 +0400

    commit

---
+++ day 12/day 12.py
@@ -0,0 +1,35 @@
+#hello <3
+
+name = input("please enter your name: ")
+last_name = input("please enter your last name: ")
+age = int(input("please enter your age: "))
+adress = input("please enter your adress: ")
+
+
+list = []
+list.append(name)
+list.append(last_name)
+list.append(age)
+list.append(adress)
+
+print(list)
+
+list2 = [name, last_name, age, adress]
+print(list [0:2])
+print(list [1:3])
+print(list [0:3])
+print(list [1:4])
+
+
+
+
+list3 = ["David", "Barbakadze"]
+print(list3 [0:1])
+print(list3 [1:2])
+
+
+
+movie_list = ["minions", "undisputed 2", "undisputed 3", "undisputed 4"]
+print(movie_list [0:1])
+print(movie_list [1:4])
+print(movie_list [1:3])
