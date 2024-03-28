1f361088b759494c7c96bb6b920f8ce512e10e44
DatunaGOA <barbakadzedata@gmail.com>
2024-03-28 18:08:38 +0400

    commit

--- day 15/day 15.py
+++ day 15/day 15.py
@@ -2,3 +2,13 @@ name = "David.Barbakadze"
 numeral = name[0:7:6]
 print(numeral)
 
+
+palindrom = "david"
+print(palindrom[::-1])
+#not a palindrom
+
+palindrom2 = "anabana"
+print(palindrom2[::-1])
+#this is a plaindrom
+
+
