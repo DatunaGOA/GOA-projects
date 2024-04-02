1e17ebad8dc3b4d2ce3858496de62eda767b21d5
DatunaGOA <barbakadzedata@gmail.com>
2024-04-02 17:02:24 +0400

    commit

--- day 15/day 15.py
+++ day 15/day 15.py
@@ -1,14 +1,58 @@
-name = "David.Barbakadze"
-numeral = name[0:7:6]
-print(numeral)
+def initial_chars(fullname):
+    splited_fullname = fullname.split(" ")
+    
 
+    firstname = splited_fullname[0]
+    lastname = splited_fullname[1]
+
+    result = firstname[0] + "." + lastname[0]
+
+    print(result)
+
+
+initial_chars("Davit Barbakadze")
+
+
+def average_arithmetic(numbers_list):
+    jami = sum(numbers_list)
+    print(jami)
+    result = jami / len(numbers_list)
+    print(result)
+
+
+average_arithmetic([1,2,3,4,5,6,7,8,9,])
 
 palindrom = "david"
 print(palindrom[::-1])
-#not a palindrom
+# not a palindrom
 
-palindrom2 = "anabana"
-print(palindrom2[::-1])
-#this is a plaindrom
+# palindrom2 = "racecar"
+# print(palindrom2[::-1])
+# #this is a plaindrom
+
+#another version
+
+def is_palindrom(word):
+    reversed_word = ""
+
+    for i in range(len(word) -1, -1, -1):
+        reversed_word = reversed_word + word[i]
+
+    print(reversed_word)
+
+
+is_palindrom("racecar")
+
+
+
+def remove_spaces(word):
+    word_without_space = ""
+    for char in word:
+        if char != " ":
+            word_without_space = word_without_space + char
+
+    print(word_without_space)
+
+remove_spaces("Goal- Oriented- Academy ")
 
 
