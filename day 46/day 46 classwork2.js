a524718971ec8375e63d02c2a80bf7aa97128299
DatunaGOA <barbakadzedata@gmail.com>
2024-07-13 10:31:38 +0400

    commit

---
+++ day 46/day 46 classwork2.js
@@ -0,0 +1,6 @@
+document.getElementById('registrationForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the form from submitting
+    
+    // Show an alert message
+    alert("You have successfully registered. Make sure the design is good.");
+});
