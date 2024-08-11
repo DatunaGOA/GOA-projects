b370c056ecde7391dff77e5a8d62bdf4c52ebd7e
DatunaGOA <barbakadzedata@gmail.com>
2024-08-11 18:56:56 +0400

    commit

---
+++ day 53/day 53 classwork.js
@@ -0,0 +1,34 @@
+// ჩვენი ორი მოცემული ცვლადი რიცხვი
+let num1 = 10;
+let num2 = 5;
+
+// დავამატოთ რიცხვები ერთმანეთთან
+let sum = num1 + num2; // რიცხვების დამატება
+console.log("Sum:", sum); // output არის 15
+
+// გამოკლება რიცხვების
+let difference = num1 - num2; // გამოკლება
+console.log("Difference:", difference); // output არის 5
+
+// რიცხვების გამრავლება
+let product = num1 * num2; // გამრავლება
+console.log("Product:", product); // output არის 50
+
+// რიცხვების გაყოფა
+let quotient = num1 / num2; // გაყოფა
+console.log("Quotient:", quotient); // output არის 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//luka teacher and batoni mate are the goats
