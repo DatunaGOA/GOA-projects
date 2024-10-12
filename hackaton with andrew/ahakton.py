06e58540fbc26091cf76368acd5bfd0c638060c3
DatunaGOA <barbakadzedata@gmail.com>
2024-10-12 21:07:19 +0400

    commit

--- hackaton with andrew/ahakton.py
+++
@@ -1,7 +0,0 @@
-function modifyWord(word) {
-    const midIndex = Math.ceil(word.length / 2); // Get the middle index
-    const firstHalf = word.slice(0, midIndex); // Get the first half
-    const secondHalf = word.slice(midIndex); // Get the second half
-    return secondHalf + firstHalf; // Concatenate second half with first half
-}
-console.log(modifyWord("gamarjoba")); // Output: "rjobagamar"
