02f5fe071637cd19e79bcd375e8bd7c215e98e5a
DatunaGOA <barbakadzedata@gmail.com>
2025-01-28 20:46:35 +0400

    commit

---
+++ day 100/classwork/classwork.js
@@ -0,0 +1,12 @@
+document.getElementById('fetchButton').addEventListener('click', function() {
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then(response => response.json())
+        .then(data => {
+            const dogImage = document.getElementById('dogImage');
+            dogImage.src = data.message;
+            dogImage.style.display = 'block';
+        })
+        .catch(error => {
+            console.error('Error fetching the dog image:', error);
+        });
+});
